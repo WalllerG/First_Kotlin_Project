9876c395cb92bee6d0e3ee31c1c576df91da3de2
Walter Guo <guowalter61@gmail.com>
2026-02-07 22:15:39 -0500

    Interfaces

--- src/PeronInfoProvider.kt
+++ src/PeronInfoProvider.kt
@@ -1,3 +1,43 @@
 interface PeronInfoProvider {
+    val providerInfo: String
+    fun printInfo(person: Person) {
+        println(providerInfo)
+        person.printName()
+    }
+}
 
+interface SessionInfoProvider {
+    val sessionId: String
+    fun getSessionId(){
+        println(sessionId)
+    }
+}
+
+
+class BasicInfoProvider : PeronInfoProvider,SessionInfoProvider {
+    override val providerInfo: String
+        get() = "BasicInfoProvider"
+
+    override val sessionId: String
+        get() = "BIP"
+
+}
+
+
+fun checkType (personInfoProvider: PeronInfoProvider) {
+    if (personInfoProvider is SessionInfoProvider) {
+        println("Is a sessionInfoProvider")
+        personInfoProvider.getSessionId()
+    }
+    else{
+        println("Is not a sessionInfoProvider")
+    }
+}
+
+
+fun main() {
+    val provider = BasicInfoProvider()
+    val person = Person("Walter","Guo")
+    provider.printInfo(person)
+    checkType(provider)
 }
